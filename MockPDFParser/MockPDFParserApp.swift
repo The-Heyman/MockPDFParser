0e426cfae9d29db821cdb3d656c0557d8612cdf1
Heyman <35301662+The-Heyman@users.noreply.github.com>
2022-02-19 07:34:09 +0100

    Initial Commit

---
+++ MockPDFParser/MockPDFParserApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockPDFParserApp.swift
+//  MockPDFParser
+//
+//  Created by Alex Gameli Heyman on 19/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MockPDFParserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
